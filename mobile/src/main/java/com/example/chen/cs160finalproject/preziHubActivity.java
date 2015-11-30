6af9d8207717d5db33efbdb09d9fdbb25930a4ec
Tammy Chen <tchen95@berkeley.edu>
2015-11-29 17:55:32 -0800

    Actual Project Files

---
+++ mobile/src/main/java/com/example/chen/cs160finalproject/preziHubActivity.java
@@ -0,0 +1,13 @@
+package com.example.chen.cs160finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class preziHubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prezi_hub);
+    }
+}
